d0a677462521c5b49bc6bce1dfc04e419d0cee06
Khoa Nguyen <khoan@datacom.co.nz>
2021-01-29 01:06:52 +1300

    Add project files.

---
+++ Fragrance.cs
@@ -0,0 +1,154 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace FragranceInfo
+{
+    public class Fragrance
+    {
+        private IWebDriver Driver;
+
+        private string URL;
+
+        public int Row; //This is for excel mapping
+
+        [Description("Brand")]
+        public string Brand;
+
+        [Description("Name")]
+        public string Name;
+
+        [Description("Owned")]
+        [ReadOnly(true)]
+        public string Owned = "No";
+
+        [Description("Sampled")]
+        [ReadOnly(true)]
+        public string Sampled = "No";
+
+        [Description("Total Votes")]
+        public int TotalVotes;
+
+        [Description("Overall Rating")]
+        public decimal Rating;
+
+        [Description("Value For Money")]
+        public decimal PriceValueRating;
+
+        [Description("Spring")]
+        public decimal SpringRating;
+
+        [Description("Summer")]
+        public decimal SummerRating;
+
+        [Description("Fall")]
+        public decimal FallRating;
+
+        [Description("Winter")]
+        public decimal WinterRating;
+
+        [Description("Day")]
+        public decimal DayRating;
+
+        [Description("Night")]
+        public decimal NightRating;
+
+        [Description("Longevity")]
+        public decimal LongevityRating;
+
+        [Description("Sillage")]
+        public decimal SillageRating;
+
+        [Description("Gender")]
+        public decimal GenderRating;
+
+        public bool IsProcessed;
+
+        public Fragrance(IWebDriver driver, string url)
+        {
+            Driver = driver;
+            URL = url;
+        }
+
+        public void GetAllAttributes()
+        {
+
+            // Brand
+            IWebElement brandElement = Driver.FindElement(By.XPath("//p[@itemprop='brand']//span[@itemprop='name']"));
+            Brand = brandElement.Text;
+            
+            // Name
+            IWebElement nameElement = Driver.FindElement(By.XPath("//h1[@itemprop = 'name']"));
+            var subElements = nameElement.FindElements(By.XPath(".//*"));
+            var text = nameElement.Text;
+           
+            foreach (IWebElement element in subElements)
+            {
+                text = text.Replace(element.Text, "");
+            }
+
+            Name = text.Trim();
+
+            // Rating
+            IWebElement ratingElement = Driver.FindElement(By.XPath("//span[@itemprop='ratingValue']"));
+            Rating = Convert.ToDecimal(ratingElement.Text);
+            
+            // Total Votes
+            var ratingCount = Driver.FindElement(By.XPath("//span[@itemprop='ratingCount']"));
+            TotalVotes = Convert.ToInt32(ratingCount.Text.Replace(",",""));
+
+            // Ratings based on seasons/day/night 
+            WinterRating = GetSeasonalRating("winter");
+            SpringRating = GetSeasonalRating("spring");
+            SummerRating = GetSeasonalRating("summer");
+            FallRating = GetSeasonalRating("fall");
+            DayRating = GetSeasonalRating("day");
+            NightRating = GetSeasonalRating("night");
+
+            // Attribute ratings
+            LongevityRating = GetAttributeRating("LONGEVITY");
+            SillageRating = GetAttributeRating("SILLAGE");
+            GenderRating = GetAttributeRating("GENDER");
+            PriceValueRating = GetAttributeRating("PRICE VALUE");
+
+            IsProcessed = true;
+        }
+
+        private decimal GetSeasonalRating(string season)
+        {
+            IWebElement ratingDiv = Driver.FindElement(By.XPath($"//span[@class='vote-button-legend'][text()='{season}']/../following-sibling::div/div/div"));
+            var style = ratingDiv.GetAttribute("style");
+            foreach (string property in style.Split(";"))
+            {
+                var pair = property.Trim().Split(":");
+
+                if (pair[0].Trim() == "width")
+                {
+                    return Convert.ToDecimal(pair[1].Trim().Replace("%", "")) / 20;
+                }
+            }
+
+            return 0;
+        }
+
+        private decimal GetAttributeRating(string attribute)
+        {
+            IList<IWebElement> levels = Driver.FindElements(By.XPath($"//span[text() = '{attribute}']/../..//span[@class='vote-button-legend']"));
+            int sum = 0;
+            int count = 0;
+
+            for (int i = 0; i < levels.Count; i++)
+            {
+                count += Convert.ToInt32(levels[i].Text);
+                sum += Convert.ToInt32(levels[i].Text) * (i + 1);
+            }
+
+            return ((decimal)sum / count) / levels.Count * 5; // some attributes have 4 levels only, need to convert to a 5 star system
+        }
+    }
+}
---
+++ FragranticaCrawler.cs
@@ -0,0 +1,222 @@
+﻿
+using OfficeOpenXml;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using LicenseContext = OfficeOpenXml.LicenseContext;
+
+namespace FragranceInfo
+{
+
+    public class FragranticaCrawler
+    {
+        const int MAX_THREAD = 2;
+        static int NUM_OF_THREADS = 0;
+        static int STARTING_ROW = 1;
+        static int STARTING_COLUMN = 1;
+        static ExcelWorksheet Worksheet;
+        static ExcelPackage Package;
+        static string URL = "URL";
+        static string Name = "Name";
+        static bool ALWAYS_PROCESS = false; // crawl the website again even if the fragrance's info is already populated
+        static bool HEADLESS = true;
+        public static void OpenExcelFile()
+        {
+            string path = @"C:\Users\khoan\Desktop\Fragance.xlsx";
+            FileInfo fileInfo = new FileInfo(path);
+
+            Package = new ExcelPackage(fileInfo);
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            Worksheet = Package.Workbook.Worksheets.FirstOrDefault();
+        }
+        public static void ProcessURLs()
+        {
+
+            OpenExcelFile();
+
+            IList<Tuple<string, int>> webs = GetURLs();
+            int total = webs.Count;
+            IList<Task<Fragrance>> tasks = new List<Task<Fragrance>>();
+            int count = 0;
+            while (tasks.Count < total)
+            {
+                if (NUM_OF_THREADS < MAX_THREAD)
+                {
+                    string web = webs[0].Item1;
+                    int row = webs[0].Item2;
+                    webs.RemoveAt(0);
+                    Interlocked.Increment(ref NUM_OF_THREADS);
+                    tasks.Add(Task.Run(() => GetFranganceInfo(web, row)));
+                    count++;
+                }
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            IList<Fragrance> fragrances = tasks.Select(x => x.Result).ToList();
+            WriteExcelFile(fragrances);
+        }
+
+        public static Fragrance GetFranganceInfo(string url, int row)
+        {
+            Console.WriteLine($"Processing {url}");
+            int attempts = 3;
+
+            ChromeOptions chromeOptions = new ChromeOptions();
+            if (HEADLESS)
+            {
+                chromeOptions.AddArguments("headless");
+            }
+
+            ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService();
+            chromeDriverService.HideCommandPromptWindow = true;
+
+            IWebDriver driver = new ChromeDriver(chromeDriverService, chromeOptions);
+
+            driver.Navigate().GoToUrl(url);
+
+            Fragrance fragance = new Fragrance(driver, url);
+            fragance.Row = row;
+
+            // Sometimes the website is not fully loaded, wait if it fails to grab any attribute. 10 times max. 
+            while (attempts > 0)
+            {
+                try
+                {
+                    Console.WriteLine($"{attempts} attempts left for {url}");
+                    fragance.GetAllAttributes();
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Thread.Sleep(1000);
+                    attempts -= 1;
+                }
+            }
+            Interlocked.Decrement(ref NUM_OF_THREADS);
+            driver.Close();
+            Console.WriteLine($"Done - {fragance.Name}. Is Processed? {fragance.IsProcessed}");
+            return fragance;
+        }
+
+        public static int FindColumnIndex(string name)
+        {
+            int lastNonEmptyColumn = GetLastNonEmptyColumnIndex();
+            int column = STARTING_COLUMN;
+
+            while (column <= lastNonEmptyColumn)
+            {
+                if (Worksheet.Cells[STARTING_ROW, column].Value?.ToString() == name)
+                {
+                    return column;
+                }
+
+                column++;
+            }
+
+            return 0;
+        }
+
+        public static IList<Tuple<string, int>> GetURLs()
+        {
+            var urlColumn = FindColumnIndex(URL);
+            var nameColumn = FindColumnIndex(Name);
+            int row = STARTING_ROW + 1;
+            IList<Tuple<string, int>> urls = new List<Tuple<string, int>>();
+            while (true)
+            {
+                ExcelRange urlCell = Worksheet.Cells[row, urlColumn];
+                var url = urlCell.Value;
+
+                ExcelRange nameCell = Worksheet.Cells[row, nameColumn];
+                var name = nameCell.Value;
+
+                if (url == null)
+                {
+                    break;
+                }
+
+                if (ALWAYS_PROCESS || name == null)
+                {
+                    urls.Add(new Tuple<string, int>(url.ToString(), row));
+                }
+                row++;
+            }
+            return urls;
+        }
+
+        public static void WriteExcelFile(IList<Fragrance> fragrances)
+        {
+            foreach (Fragrance fragrance in fragrances.Where(x => x.IsProcessed))
+            {
+                if (!fragrance.IsProcessed)
+                {
+                    continue;
+                }
+
+                IList<FieldInfo> fields = fragrance.GetType().GetFields().ToList();
+
+                foreach (FieldInfo field in fields)
+                {
+                    DescriptionAttribute descriptionAttribute = (DescriptionAttribute)field.GetCustomAttribute(typeof(DescriptionAttribute), false);
+                    ReadOnlyAttribute readonlyAttribute = (ReadOnlyAttribute)field.GetCustomAttribute(typeof(ReadOnlyAttribute), false);
+
+                    if (descriptionAttribute == null)
+                    {
+                        continue;
+                    }
+
+                    int column = FindColumnIndex(descriptionAttribute.Description);
+
+                    if (column == 0)
+                    {
+                        column = InsertEmptyColumn();
+                        Worksheet.Cells[STARTING_ROW, column].Value = descriptionAttribute.Description;
+                    }
+                    else if (readonlyAttribute != null && readonlyAttribute.IsReadOnly && Worksheet.Cells[fragrance.Row, column].Value != null) // If it's readonly and the cell already has a value, skip
+                    {
+                        continue;
+                    }
+
+                    var value = field.GetValue(fragrance);
+
+                    if (field.FieldType == typeof(decimal))
+                    {
+                        value = Math.Round((decimal)value, 2);
+                    }
+
+                    Worksheet.Cells[fragrance.Row, column].Value = value;
+                    Worksheet.Cells[Worksheet.Dimension.Address].AutoFitColumns();
+                    Package.Save();
+                }
+            }           
+        }
+
+        private static int InsertEmptyColumn()
+        {
+            int emptyColumn = FindColumnIndex(null);
+
+            if (emptyColumn > 0)
+            {
+                return emptyColumn;
+            }
+
+            emptyColumn = GetLastNonEmptyColumnIndex() + 1;
+            Worksheet.InsertColumn(emptyColumn, 1);
+
+            return emptyColumn;
+        }
+
+        private static int GetLastNonEmptyColumnIndex()
+        {
+            return Worksheet.Cells["1:1"].Last().End.Column;
+        }
+    }
+}
